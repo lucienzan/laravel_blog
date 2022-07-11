8c95a90c752c73d1b8d870fb08293bc1aaaf18f5
Danielxcott <changwilliam826@gmail.com>
2022-07-11 23:24:52 +0630

    authication, eloguent relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function post(){
+        return $this->hasOne(Post::class); //Category to post
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class); //Category to post
+    }
+
+    public function User(){
+        return $this->belongsTo(User::class); //category to user
+    }
+
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function Category(){
+        return $this->belongsTo(Category::class); //post to category
+    }
+
+    public function User(){
+        return $this->belongsTo(User::class); //post to user
+    }
 }
