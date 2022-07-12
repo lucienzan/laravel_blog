a9e5a145341d2f662876da0b3da32c3b2f9d0c59
Danielxcott <changwilliam826@gmail.com>
2022-07-12 21:33:43 +0630

    added home page view

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,9 +19,12 @@ use App\Http\Controllers\PhotoController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[PageController::class,'index'])->name('page.index');
+Route::get('/detail/{slug}',[PageController::class,'detail'])->name('page.detail');
+Route::get("/cat/{category:slug}",[PageController::class,'postByCategory'])->name('page.category');
 
 Auth::routes();
 
